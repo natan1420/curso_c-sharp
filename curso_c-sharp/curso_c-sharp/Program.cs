b5744e242151844143d0dbdf755a886853624841
Natanael Torres <natanael1420@hotmail.com>
2020-04-10 15:08:25 -0300

    Projeto Criado
    Criei o meu projeto e add, as linhas de programação da primeira aula que fiz anteriormente

---
+++ curso_c-sharp/curso_c-sharp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace curso_c_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaração de variáveis e formas de declarar
+            long i = 123;
+            double d = 1111.111;
+
+
+            var a = "Olá";
+            double g = 0;
+
+            //Mannipulação dos resultados
+            Console.WriteLine(i);
+            Console.WriteLine(Math.Round(d, 2));
+            Console.WriteLine(i + d);
+            Console.WriteLine(g);
+            Console.WriteLine(a);
+
+            //conversão de variáveis
+            t = Convert.ToString(d);
+        }
+    }
+}
